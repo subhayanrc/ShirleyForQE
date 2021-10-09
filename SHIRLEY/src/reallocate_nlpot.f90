36cb181bb63506b278488d37575a523fc49cc5a0
David Prendergast <dgprendergast@lbl.gov>
2021-10-08 22:11:30 -0700

    Copied from allocate_nlpot (again) but with a small change for the definition of nqx - the size of the tab_at Fourier grid, since LDA+U can occur at q+G

--- SHIRLEY/src/reallocate_nlpot.f90
+++ SHIRLEY/src/reallocate_nlpot.f90
@@ -1,13 +1,16 @@
 !
-! Copyright (C) 2001-2007 Quantum ESPRESSO group
+! Copyright (C) 2001-2012 Quantum ESPRESSO group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
 !
+! davegp - this routine is a copy of allocate_nlpot (PW/src) which instead
+!          of just allocating these data structures, it first checks in
+!          case they are already allocated. 
 !-----------------------------------------------------------------------
-subroutine reallocate_nlpot
+SUBROUTINE reallocate_nlpot
   !-----------------------------------------------------------------------
   !
   ! This routine computes the dimension of the Hamiltonian matrix and
@@ -15,101 +18,123 @@ subroutine reallocate_nlpot
   !
   ! It computes the following global quantities:
   !
-  !     ngk           !  number of plane waves (for each k point)
   !     npwx          !  maximum number of plane waves
   !     nqx           !  number of points of the interpolation table
   !     nqxq          !  as above, for q-function interpolation table
   !
   !
+
+  USE control_flags, ONLY : tqr
   USE ions_base,        ONLY : nat, nsp, ityp
-  USE cell_base,        ONLY : tpiba2
   USE cellmd,           ONLY : cell_factor
   USE gvect,            ONLY : ngm, gcutm, g
-  use gvecw,            only : ecutwfc
-  USE klist,            ONLY : xk, wk, ngk, nks, qnorm, igk_k
+  USE klist,            ONLY : xk, wk, nks, qnorm
   USE lsda_mod,         ONLY : nspin
   USE ldaU,             ONLY : Hubbard_lmax
   USE scf,              ONLY : rho
   USE noncollin_module, ONLY : noncolin
-  USE wvfct,            ONLY : npwx, npw, g2kin
+  USE wvfct,            ONLY : npwx, g2kin
+  USE gvecw,            ONLY : gcutw, ecutwfc
   USE us,               ONLY : qrad, tab, tab_d2y, tab_at, dq, nqx, &
                                nqxq, spline_ps
-  USE uspp,             ONLY : indv, nhtol, nhtolm, ijtoh, qq_at, dvan, deeq, vkb, &
-                               nkb, nkbus, nhtoj, becsum, qq_so, dvan_so, deeq_nc
+  USE uspp,             ONLY : indv, nhtol, nhtolm, ijtoh, qq_at, qq_nt, dvan, deeq, &
+                               vkb, indv_ijkb0, okvan, nkb, nkbus, nhtoj, &
+                               becsum, ebecsum, qq_so,dvan_so, deeq_nc
   USE uspp_param,       ONLY : upf, lmaxq, lmaxkb, nh, nhm, nbetam
   USE spin_orb,         ONLY : lspinorb, fcoef
-  USE paw_variables,    ONLY : okpaw
+  use cell_base,        only : tpiba
   !
-  implicit none
+  IMPLICIT NONE
   !
-  !    a few local variables
-  !
-  integer :: nwfcm  
-  ! counters on atom type, atoms, beta functions
+  INTEGER, EXTERNAL :: n_plane_waves
+  INTEGER :: nwfcm
   !
   !   calculate number of PWs for all kpoints
   !
-  if( allocated( ngk ) ) deallocate( ngk ); allocate (ngk( nks ))
+  npwx = n_plane_waves (gcutw, nks, xk, g, ngm)
   !
-  call n_plane_waves (ecutwfc, tpiba2, nks, xk, g, ngm, npwx, ngk)
+  !   g2kin contains the kinetic energy \hbar^2(k+G)^2/2m
   !
-  !   igk_k relates the index of PW k+G to index in the list of G vector
-  !
-  if( allocated( igk_k ) ) deallocate( igk_k ) ; allocate (igk_k( npwx, nks ) )
-  if( allocated( g2kin ) ) deallocate( g2kin ); allocate ( g2kin ( npwx ) )    
+  if( allocated(g2kin) ) deallocate(g2kin)
+  ALLOCATE (g2kin ( npwx ) )
   !
   ! Note: computation of the number of beta functions for
   ! each atomic type and the maximum number of beta functions
   ! and the number of beta functions of the solid has been
   ! moved to init_run.f90 : pre_init()
   !
-  if( allocated(indv) ) deallocate(indv); allocate (indv( nhm, nsp))    
-  if( allocated(nhtol) ) deallocate(nhtol); allocate (nhtol(nhm, nsp))    
-  if( allocated(nhtolm) ) deallocate(nhtolm); allocate (nhtolm(nhm, nsp))    
-  if( allocated(nhtoj) ) deallocate(nhtoj); allocate (nhtoj(nhm, nsp))    
-  if( allocated(ijtoh) ) deallocate(ijtoh); allocate (ijtoh(nhm, nhm, nsp))
-  if( allocated(deeq) ) deallocate(deeq); allocate (deeq( nhm, nhm, nat, nspin))    
-  if (noncolin) then
-     if( allocated(deeq_nc) ) deallocate(deeq_nc); allocate (deeq_nc( nhm, nhm, nat, nspin))    
-  endif
-  if( allocated(qq_at) ) deallocate(qq_at); allocate (qq_at(   nhm, nhm, nsp))    
-  if (lspinorb) then
-    if( allocated(qq_so) ) deallocate(qq_so); allocate (qq_so(nhm, nhm, 4, nsp))    
-    if( allocated(dvan_so) ) deallocate(dvan_so); allocate (dvan_so( nhm, nhm, nspin, nsp))    
-    if( allocated(fcoef) ) deallocate(fcoef); allocate (fcoef(nhm,nhm,2,2,nsp))
-  else
-    if( allocated(dvan) ) deallocate(dvan); allocate (dvan( nhm, nhm, nsp))    
-  endif
+  if( allocated(indv) ) deallocate(indv)
+  if( allocated(nhtol) ) deallocate(nhtol)
+  if( allocated(nhtolm) ) deallocate(nhtolm)
+  if( allocated(nhtoj) ) deallocate(nhtoj)
+  if( allocated(ijtoh) ) deallocate(ijtoh)
+  if( allocated(indv_ijkb0) ) deallocate(indv_ijkb0)
+  if( allocated(deeq) ) deallocate(deeq)
+  if( allocated(deeq_nc) ) deallocate(deeq_nc)
+  if( allocated(qq_at) ) deallocate(qq_at)
+  if( allocated(qq_nt) ) deallocate(qq_nt)
+  if( allocated(qq_so) ) deallocate(qq_so)
+  if( allocated(dvan_so) ) deallocate(dvan_so)
+  if( allocated(fcoef) ) deallocate(fcoef)
+  if( allocated(dvan) ) deallocate(dvan)
+  !
+  ALLOCATE (indv( nhm, nsp))
+  ALLOCATE (nhtol(nhm, nsp))
+  ALLOCATE (nhtolm(nhm, nsp))
+  ALLOCATE (nhtoj(nhm, nsp))
+  ALLOCATE (ijtoh(nhm, nhm, nsp))
+  ALLOCATE (indv_ijkb0(nat))
+  ALLOCATE (deeq( nhm, nhm, nat, nspin))
+  IF (noncolin) THEN
+     ALLOCATE (deeq_nc( nhm, nhm, nat, nspin))
+  ENDIF
+  ALLOCATE (qq_at( nhm, nhm, nat))
+  ALLOCATE (qq_nt( nhm, nhm, nsp))
+  IF (lspinorb) THEN
+    ALLOCATE (qq_so(nhm, nhm, 4, nsp))
+    ALLOCATE (dvan_so( nhm, nhm, nspin, nsp))
+    ALLOCATE (fcoef(nhm,nhm,2,2,nsp))
+  ELSE
+    ALLOCATE (dvan( nhm, nhm, nsp))
+  ENDIF
+  ! GIPAW needs a slighly larger q-space interpolation for quantities calculated
+  ! at k+q_gipaw, and I'm using the spline_ps=.true. flag to signal that
+  IF (spline_ps .and. cell_factor <= 1.1d0) cell_factor = 1.1d0
   !
   ! This routine is called also by the phonon code, in which case it should
   ! allocate an array that includes q+G vectors up to |q+G|_max <= |Gmax|+|q|
   !
-  nqxq = INT( ( (sqrt(gcutm) + qnorm ) / dq + 4) * cell_factor )
+  nqxq = int( ( (sqrt(gcutm) + qnorm) / dq + 4) * cell_factor )
   lmaxq = 2*lmaxkb+1
   !
-  if (lmaxq > 0) then
-    if( allocated(qrad) ) deallocate(qrad); allocate (qrad( nqxq, nbetam*(nbetam+1)/2, lmaxq, nsp))    
-  endif
-  if (nkb > 0) then
-    if( allocated(vkb) ) deallocate(vkb); allocate (vkb( npwx,  nkb))    
-  endif
-  if( allocated(becsum) ) deallocate(becsum); allocate (becsum( nhm * (nhm + 1)/2, nat, nspin))    
+  if( allocated(qrad) ) deallocate(qrad)
+  if( allocated(vkb) ) deallocate(vkb)
+  if( allocated(becsum) ) deallocate(becsum)
+  if( allocated(ebecsum) ) deallocate(ebecsum)
+  !
+  IF (lmaxq > 0) ALLOCATE (qrad( nqxq, nbetam*(nbetam+1)/2, lmaxq, nsp))
+  ALLOCATE (vkb( npwx,  nkb))
+  ALLOCATE (becsum( nhm * (nhm + 1)/2, nat, nspin))
+  if (tqr) ALLOCATE (ebecsum( nhm * (nhm + 1)/2, nat, nspin))
   !
   ! Calculate dimensions for array tab (including a possible factor
   ! coming from cell contraction during variable cell relaxation/MD)
+  ! davegp - added tpiba to include q+G length - could be more rigorous
   !
-  nqx = INT( (sqrt (ecutwfc) / dq + 4) * cell_factor )
+  nqx = int( ((sqrt (ecutwfc) + tpiba*2.d0) / dq + 4) * cell_factor )
 
-  if( allocated(tab) ) deallocate(tab); allocate (tab( nqx , nbetam , nsp))
+  if( allocated(tab) ) deallocate(tab)
+  if( allocated(tab_d2y) ) deallocate(tab_d2y)
+  if( allocated(tab_at) ) deallocate(tab_at)
+
+  ALLOCATE (tab( nqx , nbetam , nsp))
 
   ! d2y is for the cubic splines
-  if (spline_ps) then
-    if( allocated(tab_d2y) ) deallocate(tab_d2y); allocate (tab_d2y( nqx , nbetam , nsp))
-  endif
+  IF (spline_ps) ALLOCATE (tab_d2y( nqx , nbetam , nsp))
 
-  nwfcm = MAXVAL ( upf(1:nsp)%nwfc )
-  if( allocated(tab_at) ) deallocate(tab_at); allocate (tab_at( nqx , nwfcm , nsp))
+  nwfcm = maxval ( upf(1:nsp)%nwfc )
+  ALLOCATE (tab_at( nqx , nwfcm , nsp))
 
-  return
-end subroutine reallocate_nlpot
+  RETURN
+END SUBROUTINE reallocate_nlpot
 
