077b834d66dff2b56ffc3eb8ca06bbba89fea547
David Prendergast <dgprendergast@lbl.gov>
2021-10-08 22:08:30 -0700

    Updated version of deallocate_fft.f90 modeled after PW/src

--- SHIRLEY/src/deallocate_fft.f90
+++ SHIRLEY/src/deallocate_fft.f90
@@ -1,53 +1,43 @@
 !
-! Copyright (C) 2001 PWSCF group
+! Copyright (C) 2001-2010 Quantum ESPRESSO group
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
 !
+! davegp - copied from PW/src/allocate_fft
 !-----------------------------------------------------------------------
-subroutine deallocate_fft
+SUBROUTINE deallocate_fft
   !-----------------------------------------------------------------------
-  !     This routine deallocates the data structure associated to the FFT
   !
-!#include "f_defs.h"
-  USE gvect,     ONLY : deallocate_gvect
-  USE scf,       ONLY : rho, v, vltot, vrs, vnew, rho_core, rhog_core, kedtau
-  use scf,       only : destroy_scf_type
-  USE control_flags,    ONLY : gamma_only
-  USE noncollin_module, ONLY : pointlist, factlist, r_loc, &
-      report, i_cons, noncolin
+  !     This routine *de*allocates memory for FFT-related arrays - IMPORTANT:
+  !     routine "data_structure" must be called before it in order to
+  !     set the proper dimensions and grid distribution across processors
+  !     these dimensions
+  !
+  USE scf,       ONLY : rho, v, vnew, vltot, vrs, rho_core, rhog_core, &
+                        kedtau, destroy_scf_type
+  USE noncollin_module, ONLY : pointlist, factlist, r_loc
   USE wavefunctions, ONLY : psic, psic_nc
-  implicit none
+  IMPLICIT NONE
   !
-  !     DeAllocate memory for all kind of stuff.
+  !     *DE*Allocate memory for all kind of stuff.
   !
-  call deallocate_gvect()
-  call destroy_scf_type( rho )
-  call destroy_scf_type( v )
-  call destroy_scf_type( vnew )
-  if( allocated( vltot ) ) deallocate (vltot)    
-  if( allocated( vrs ) ) deallocate (vrs)    
-  if( allocated( rho_core ) ) deallocate (rho_core)    
-  if( allocated( kedtau ) ) deallocate (kedtau)    
-  if( allocated( rhog_core ) ) deallocate (rhog_core)    
+  CALL destroy_scf_type(rho)
+  CALL destroy_scf_type(v)
+  CALL destroy_scf_type(vnew)
+  if( allocated(vltot) ) deallocate(vltot)
+  if( allocated(rho_core) ) deallocate(rho_core)
+  if( allocated(kedtau) ) deallocate(kedtau)
+  if( allocated(rhog_core) ) deallocate(rhog_core)
+  if( allocated(psic) ) deallocate(psic)
+  if( allocated(vrs) ) deallocate(vrs)
 
-  if( allocated( psic ) ) deallocate (psic)    
-  if (noncolin) then
-     if( allocated( psic_nc ) ) deallocate (psic_nc)    
-     if (((report.ne.0).or.(i_cons.ne.0)).and.(noncolin)) then
-!
-! In order to print out local quantities, integrated around the atoms,
-! we need the following variables
-!
-        if( allocated( pointlist ) ) deallocate(pointlist)
-        if( allocated( factlist ) ) deallocate(factlist)
-        if( allocated( r_loc ) ) deallocate(r_loc)
+  if( allocated(psic_nc) ) deallocate(psic_nc)
+  if( allocated(pointlist) ) deallocate(pointlist)
+  if( allocated(factlist) ) deallocate(factlist)
+  if( allocated(r_loc) ) deallocate(r_loc)
 
-     endif
-  endif
-
-  return
-end subroutine deallocate_fft
-
+  RETURN
+END SUBROUTINE deallocate_fft
